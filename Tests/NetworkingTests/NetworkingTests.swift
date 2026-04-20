d84e0dc4e3934e34ec975e5e515031754bcc4930
Connor Gibbons <connorgibbons@comcast.net>
2026-04-20 14:03:13 -0400

    Added a non-async test for TCPServer.

--- Tests/NetworkingTests/NetworkingTests.swift
+++ Tests/NetworkingTests/NetworkingTests.swift
@@ -19,10 +19,10 @@ final class NetworkingTests: XCTestCase {
     func testTCPServer() async throws {
         let connectionName: Wrapped<String> = .init(value: "")
         let (stream, continuation) = AsyncStream.makeStream(of: String.self)
-        let server = try TCPServer(port: 4242, maxConnections: 1, actionOnNewConnection: { connectionName.update(value: $0.connectionName) }, actionOnReceive: { _, data in
+        let server = try await TCPServer(port: 55301, maxConnections: 1, actionOnNewConnection: { connectionName.update(value: $0.connectionName) }, actionOnReceive: { _, data in
             continuation.yield(String(data: data, encoding: .ascii)!)
         }) // The warning here is invalid, don't actually replace with '_', it'll deinit the object and fail the test
-        let newConnection = try await TCPConnection(onRead: {_ in }, host: "0.0.0.0", port: 4242)
+        let newConnection = try await TCPConnection(onRead: {_ in }, host: "0.0.0.0", port: 55301)
         for n in 0..<102 {
             try await newConnection.sendLine("meow! \(n)")
         }
@@ -32,6 +32,21 @@ final class NetworkingTests: XCTestCase {
             print("Received: \(received) from: \(connectionName.getValue()), \(recCount)")
             if(recCount >= 2) { break }
         }
+    }
+    
+    func testTCPServerNonAsync() throws {
+        let connectionName: Wrapped<String> = .init(value: "")
+        let received: Wrapped<[String]> = .init(value: [])
+        let sem = DispatchSemaphore(value: 0)
+        let server = try TCPServer(port: 55302, maxConnections: 1, actionOnNewConnection: { connectionName.update(value: $0.connectionName) }, actionOnReceive: { _,data in received.value.append(String(data: data, encoding: .ascii)!); sem.signal() })
+        let newConnection = try TCPConnection(onRead: {_ in }, host: "0.0.0.0", port: 55302)
+        for n in 0..<102 {
+            try newConnection.send(Data("meow! \(n)".utf8))
+        }
+        for _ in 0..<5 {
+            sem.wait()
+        }
+        print(received.getValue())
     }
     
     
